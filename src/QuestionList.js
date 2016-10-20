7c303bcad800e33ae4c89a3046aa3cf72cebe290
Edward Andrews-Hodgson <edward@aldreth.com>
2016-10-20 20:39:56 +0100

    Add some questions

---
+++ src/QuestionList.js
@@ -0,0 +1,54 @@
+const QuestionList = {
+  q1: {
+    question: 'Is it an oak?',
+    answers: [
+      {
+        answer: 'Yes'
+      },
+      {
+        answer: 'No',
+        nextKey: 'q2'
+      },
+    ]
+  },
+  q2: {
+    question: 'Is it an ash or a horse chestnut?',
+    answers: [
+      {
+        answer: 'An ash'
+      },
+      {
+        answer: 'A horse chestnut'
+      },
+      {
+        answer: 'Neither',
+        nextKey: 'q3'
+      }
+    ]
+  },
+  q3: {
+    question: 'Is it evergreen',
+    answers: [
+      {
+        answer: 'No',
+        nextKey: 'q4'
+      },
+      {
+        answer: 'Yes',
+        nextKey: 'q5'
+      },
+    ]
+  },
+  q4: {
+    answers: [
+      {
+        answer: 'It is a cedar'
+      },
+      {
+        answer: 'It is a sycamore'
+      },
+    ]
+  }
+}
+
+export default QuestionList;
