537bccd744f8f8928eac74b1ef0de99286389336
Edward Andrews-Hodgson <edward@aldreth.com>
2016-10-23 17:13:59 +0100

    Make App a stateless component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import createStore from './store';
-import App from './App';
+import App from './components/App';
 import './index.css';
 
 const store = createStore();
